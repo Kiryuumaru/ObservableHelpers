f1db62ef0d6c363ac2b852a6304864d34dedd55d
Kiryuumaru <kiryuumaru@outlook.com>
2021-06-05 23:03:15 +0800

    Fixed base SynchronizationContext

--- ObservableHelpers/SyncContext.cs
+++ ObservableHelpers/SyncContext.cs
@@ -9,31 +9,37 @@ namespace ObservableHelpers
 {
     public abstract class SyncContext : Disposable
     {
-        private SynchronizationContext context;
+        private Func<SynchronizationContext> contextFactory;
 
         protected SyncContext()
         {
             SetSynchronizationContext();
         }
 
-        protected void SynchronizationContextPost(Action action)
-        {
-            context.Post(s => action(), null);
-        }
-
         public void SetSynchronizationContext()
         {
-            context = AsyncOperationManager.SynchronizationContext;
+            SynchronizationContext context = AsyncOperationManager.SynchronizationContext;
+            contextFactory = new Func<SynchronizationContext>(() => context);
         }
 
         public void SetSynchronizationContext(SynchronizationContext context)
         {
-            this.context = context;
+            contextFactory = new Func<SynchronizationContext>(() => context);
         }
 
         public void SetSynchronizationContext(SyncContext syncContext)
         {
-            context = syncContext.context;
+            contextFactory = new Func<SynchronizationContext>(() => syncContext.contextFactory.Invoke());
+        }
+
+        public void SetSynchronizationContext(Func<SynchronizationContext> contextFactory)
+        {
+            this.contextFactory = new Func<SynchronizationContext>(() => contextFactory.Invoke());
+        }
+
+        protected void SynchronizationContextPost(Action action)
+        {
+            contextFactory.Invoke().Post(s => action(), null);
         }
     }
 }
