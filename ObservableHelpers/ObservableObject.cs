c26fbfd28fe168bc5d8694a27232281776c4ba26
Kiryuumaru <kiryuumaru@outlook.com>
2021-06-10 10:09:43 +0800

    Bug fixes

--- ObservableHelpers/ObservableObject.cs
+++ ObservableHelpers/ObservableObject.cs
@@ -11,97 +11,6 @@ using System.Threading.Tasks;
 
 namespace ObservableHelpers
 {
-    #region Helpers
-
-    public class PropertyHolder
-    {
-        public object Property { get; set; }
-        public string Key { get; set; }
-        public string PropertyName { get; set; }
-        public string Group { get; set; }
-
-        public bool CopyToSelf(object property, string key, string propertyName, string group)
-        {
-            var hasChanges = false;
-            if (Key != key)
-            {
-                Key = key;
-                hasChanges = true;
-            }
-
-            if (PropertyName != propertyName)
-            {
-                PropertyName = propertyName;
-                hasChanges = true;
-            }
-
-            if (Group != group)
-            {
-                Group = group;
-                hasChanges = true;
-            }
-
-            if (!Property?.Equals(property) ?? property != null)
-            {
-                Property = property;
-                hasChanges = true;
-            }
-            return hasChanges;
-        }
-
-        public bool CopyToSelf(PropertyHolder propertyHolder)
-        {
-            return CopyToSelf(propertyHolder.Property, propertyHolder.Key, propertyHolder.PropertyName, propertyHolder.Group);
-        }
-
-        public override bool Equals(object obj) => Equals(obj as PropertyHolder);
-
-        public bool Equals(PropertyHolder prop)
-        {
-            if (prop is null)
-            {
-                return false;
-            }
-
-            if (ReferenceEquals(this, prop))
-            {
-                return true;
-            }
-
-            if (GetType() != prop.GetType())
-            {
-                return false;
-            }
-
-            return
-                (Key == prop.Key) &&
-                (PropertyName == prop.PropertyName) &&
-                (Group == prop.Group) &&
-                (Property?.Equals(prop.Property) ?? prop.Property == null);
-        }
-
-        public override int GetHashCode() => (Key, PropertyName, Group, Property).GetHashCode();
-
-        public static bool operator ==(PropertyHolder left, PropertyHolder right)
-        {
-            if (left is null)
-            {
-                if (right is null)
-                {
-                    return true;
-                }
-
-                return false;
-            }
-            return left.Equals(right);
-        }
-
-        public static bool operator !=(PropertyHolder lhs, PropertyHolder rhs) => !(lhs == rhs);
-
-    }
-
-    #endregion
-
     public class ObservableObject : Observable
     {
         #region Properties
@@ -179,8 +88,6 @@ namespace ObservableHelpers
             [CallerMemberName] string propertyName = null,
             string group = null)
         {
-            VerifyNotDisposed();
-
             return SetPropertyInternal(value, null, propertyName, group);
         }
 
@@ -190,8 +97,6 @@ namespace ObservableHelpers
             [CallerMemberName] string propertyName = null,
             string group = null)
         {
-            VerifyNotDisposed();
-
             return SetPropertyInternal(value, key, propertyName, group);
         }
 
@@ -200,8 +105,6 @@ namespace ObservableHelpers
             [CallerMemberName] string propertyName = null,
             string group = null)
         {
-            VerifyNotDisposed();
-
             return GetPropertyInternal(defaultValue, null, propertyName, group);
         }
 
@@ -211,51 +114,17 @@ namespace ObservableHelpers
             [CallerMemberName] string propertyName = null,
             string group = null)
         {
-            VerifyNotDisposed();
-
             return GetPropertyInternal(defaultValue, key, propertyName, group);
         }
 
         protected virtual bool DeleteProperty(string propertyName)
         {
-            VerifyNotDisposed();
-
-            PropertyHolder propHolder = null;
-            lock (propertyHolders)
-            {
-                propHolder = propertyHolders.FirstOrDefault(i => i.PropertyName == propertyName);
-            }
-            if (propHolder == null) return false;
-            if (propHolder.Property != null)
-            {
-                propHolder.Property = null;
-                return true;
-            }
-            else
-            {
-                return false;
-            }
+            return Delete(null, propertyName);
         }
 
         protected virtual bool DeletePropertyWithKey(string key)
         {
-            VerifyNotDisposed();
-
-            PropertyHolder propHolder = null;
-            lock (propertyHolders)
-            {
-                propHolder = propertyHolders.FirstOrDefault(i => i.Key == key);
-            }
-            if (propHolder == null) return false;
-            if (propHolder.Property != null)
-            {
-                propHolder.Property = null;
-                return true;
-            }
-            else
-            {
-                return false;
-            }
+            return Delete(key, null);
         }
 
         protected IEnumerable<PropertyHolder> GetRawProperties(string group = null)
---
+++ ObservableHelpers/PropertyHolder.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ObservableHelpers
+{
+    public class PropertyHolder
+    {
+        public object Property { get; set; }
+        public string Key { get; set; }
+        public string PropertyName { get; set; }
+        public string Group { get; set; }
+
+        public bool UpdateProperty(object property)
+        {
+            if (!(Property?.Equals(property) ?? property == null))
+            {
+                Property = property;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool CopyToSelf(object property, string key, string propertyName, string group)
+        {
+            var hasChanges = false;
+            if (Key != key)
+            {
+                Key = key;
+                hasChanges = true;
+            }
+
+            if (PropertyName != propertyName)
+            {
+                PropertyName = propertyName;
+                hasChanges = true;
+            }
+
+            if (Group != group)
+            {
+                Group = group;
+                hasChanges = true;
+            }
+
+            if (UpdateProperty(property))
+            {
+                Property = property;
+                hasChanges = true;
+            }
+            return hasChanges;
+        }
+
+        public bool CopyToSelf(PropertyHolder propertyHolder)
+        {
+            return CopyToSelf(propertyHolder.Property, propertyHolder.Key, propertyHolder.PropertyName, propertyHolder.Group);
+        }
+
+        public override bool Equals(object obj) => Equals(obj as PropertyHolder);
+
+        public bool Equals(PropertyHolder prop)
+        {
+            if (prop is null)
+            {
+                return false;
+            }
+
+            if (ReferenceEquals(this, prop))
+            {
+                return true;
+            }
+
+            if (GetType() != prop.GetType())
+            {
+                return false;
+            }
+
+            return
+                (Key == prop.Key) &&
+                (PropertyName == prop.PropertyName) &&
+                (Group == prop.Group) &&
+                (Property?.Equals(prop.Property) ?? prop.Property == null);
+        }
+
+        public override int GetHashCode() => (Key, PropertyName, Group, Property).GetHashCode();
+
+        public static bool operator ==(PropertyHolder left, PropertyHolder right)
+        {
+            if (left is null)
+            {
+                if (right is null)
+                {
+                    return true;
+                }
+
+                return false;
+            }
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(PropertyHolder lhs, PropertyHolder rhs) => !(lhs == rhs);
+
+    }
+}
